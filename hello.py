871e920cba8b7b383b94459997c63fa241230b3e
Ghaida Al-Atoum <ghaida.alatoum2@gmail.com>
2024-04-25 21:12:33 -0500

    [TEMP]

--- hello.py
+++ hello.py
@@ -4,8 +4,18 @@ import sys
 import tensorflow as tf
 import tensorflow.keras
 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
 print(f"Python Platform: {platform.platform()}")
 print(f"Tensor Flow Version: {tf.__version__}")
 print(f"Keras Version: {tensorflow.keras.__version__}")
 print()
 print(f"Python {sys.version}")
+print("==============================================")
+print(os.system("pip list | grep tensor"))
+print("==============================================")
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    print(gpu)
